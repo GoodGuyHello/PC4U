abdee222cf2e59982e35d7fbb15240aff527e052
GoodGuyHello <kn664@drexel.edu>
2025-11-19 12:47:16 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("show-budget-btn").addEventListener("click", () => {
-    document.getElementById("intro-wrapper").style.display = "none";
-
-    const questionnaire = document.getElementById("questionnaire");
-    questionnaire.classList.add("show-questionnaire");
-
-    showStep(1);
-});
